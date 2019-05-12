71c97d5cfddcc7bfd6ea3743ee3ccb101ec1a812
soon <soon@soonui-MacBook-Pro.local>
2019-05-12 16:06:48 +0900

    add web front

---
+++ chat_front/app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+const axios = require('axios');
+
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log('connected: %s sockets connected', connections.length);
+    
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    })
+
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg: data});
+    });
+
+    socket.on('send message', function(data){
+        axios.get('http://:')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    });
+});
