6fe7d53d649157b61367641cb5a6d1a083c90dd9
onting <hemool123@naver.com>
2021-05-29 13:24:38 +0900

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div class="content">
+        <ul role="listbox">
+          
+        </ul>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="App">
+      <header>
+        <h1>KW Chat</h1>
       </header>
+      <Content />
+      <footer>
+        <input type="text" />
+        <button>Put</button>
+      </footer>
     </div>
   );
 }
